e12c553981b39c843f983464531b684095b01927
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 13:42:27 +0100

    solve chapter 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
@@ -10,6 +11,7 @@ const routes: Routes = [
     component: DashboardComponent,
   },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,17 +10,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  selectedHero?: Hero;
-
   heroes$: Observable<Hero[]> = this.heroService.getHeroes();
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService,
-  ) {}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) {}
 }
