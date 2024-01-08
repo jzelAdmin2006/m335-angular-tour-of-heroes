a8729369a5521954b87e602185cdacf547f29f4b
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 12:01:01 +0100

    add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent, HeroesComponent],
+  declarations: [AppComponent, TestComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
