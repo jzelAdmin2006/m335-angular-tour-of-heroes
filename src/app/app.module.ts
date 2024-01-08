98acde4954045c6f604ed947728d9af34722f9b3
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 10:44:06 +0100

    create heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
