017b82ecbba19692b51728b78ff022c0b298a8bc
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 12:57:52 +0100

    make heroes fetching async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,34 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   selectedHero?: Hero;
 
-  heroes: Hero[] = [];
+  heroes$: Observable<Hero[]> = this.heroService.getHeroes();
 
   constructor(
     private heroService: HeroService,
     private messageService: MessageService,
   ) {}
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
