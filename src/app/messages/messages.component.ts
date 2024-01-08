70d194e79b6e1d9a00cd9c3593fdeefab8f5581c
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 14:54:45 +0100

    add missing imports

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../message.service';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss'],
   standalone: true,
+  imports: [NgIf, NgForOf],
 })
 export class MessagesComponent {
   constructor(public messageService: MessageService) {}
