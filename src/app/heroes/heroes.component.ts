c8c23a49c798b871f7bdd663f7f893185b29d81e
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-08 11:22:16 +0100

    Revert "Display heroes from mock"
    This reverts commit 927c6ecf2f35bd7fc5f0a5628954d467786230e1.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,5 +7,8 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
 }
